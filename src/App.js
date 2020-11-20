d82675f601a07b45f55cb49c490bfdadae75434b
leeyang2019 <nyang@madisoncollege.edu>
2020-11-20 00:10:37 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
 	return (
 		<>
-			<h1>Welcome to Proshop</h1>
+			<main>
+				<Header />
+				<Footer />
+			</main>
 		</>
 	);
 };
